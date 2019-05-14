3f9ca870de31252394c79a911d47b6c8467601e6
Igor Aragão <roginaldosemog@gmail.com>
2019-05-13 23:43:16 -0300

    Adiciona superuser
    Co-authored-by: willallmeida <will.allmeida@gmail.com>
    Co-authored-by: Vinícius Cantuária <cantuariavc@gmail.com>
    Co-authored-by: saraivinha97 <geovannessaraiva97@gmail.com>

--- src/unbind/urls.py
+++ src/unbind/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import url,include
 from django.urls import path
 
 from principal.views import index
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
     path('', index, name='index'),
 ]
