f327db5d6cb2c2a3648c88351718095dc3e9b20a
saraivinha97 <geovannessaraiva97@gmail.com>
2019-05-26 22:28:21 -0300

    adiciona facade got
    Co-authored-by: WillAllmeida <will.allmeida@gmail.com>
    Co-authored-by: aquiles23 <jjoseaquiless@gmail.com>
    Co-authored-by: jpmota2208 <jpmota.unb@gmail.com>
    Co-authored-by: igorveludo <velvet.guimaraes@gmail.com>

--- src/principal/urls.py
+++ src/principal/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
-from usuario import views
+from principal import views
 
 #templates tagging
 #app_name = 'principal_app'
 
 urlpatterns = [
-    url(r'register/$',views.register,name='register'),
+    url(r'^$',views.index,name='index'),
 ]
