302914195389742ed0f210ac058313f8c2a427e4
aquiles <jjoseaquiless@gmail.com>
2019-06-10 09:29:52 -0300

    add fake_gen no requirements e coserta view de usuario_artigo

--- src/artigo/views.py
+++ src/artigo/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from .models import Artigo
+from usuario.models import Usuario
 
 def usuario_artigo(request):
     user_atual = request.user
     arr = []
-    for i in Artigo.objects.filter(usuario=user_atual.id):
+    us = Usuario.objects.filter(user = user_atual.id)
+    for i in Artigo.objects.filter(usuario=us[0].id):
         arr.append(i.titulo)
     context = {'artigos': arr}
     return render(request, 'usuario_artigo.html',context)
