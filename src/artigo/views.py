2b7d96a80f62ee2385866232305cccc833d7f7c8
aquiles <jjoseaquiless@gmail.com>
2019-06-09 23:04:44 -0300

    realiza exibição de artigos

--- src/artigo/views.py
+++ src/artigo/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from .models import Artigo
 
 def usuario_artigo(request):
-    return render(request, 'usuario_artigo.html')
+    user_atual = request.user
+    arr = []
+    for i in Artigo.objects.filter(usuario=user_atual.id):
+        arr.append(i.titulo)
+    context = {'artigos': arr}
+    return render(request, 'usuario_artigo.html',context)
