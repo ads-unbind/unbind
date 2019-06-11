6fa4c7f0371971c2b1c2dd5d9b8573b219fb9d99
IgorVeludo <velvet.guimaraes@gmail.com>
2019-06-11 20:51:41 -0300

    Cria html e js para apresentacao e filtragem das categorias
    Co-authored-by: jpmota2208 <jpmota.unb@gmail.com>

--- src/categoria/views.py
+++ src/categoria/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 
+from categoria.models import Categoria
+
 
 def list(request):
-    return render(request, 'categorias.html')
+    """
+    Listagem de Categorias
+    :param request: requisição http
+    :return: lista de categorias
+    """
+    categorias = Categoria.objects.all()
+    context = {
+        "categorias": categorias
+    }
+    return render(request, 'categorias.html', context)
