86cb893ccb61dd0c825e0a9003e755488a611dc3
aquiles <jjoseaquiless@gmail.com>
2019-06-22 23:12:28 -0300

    cria form para campo de pontos nas perguntas

--- src/questionario/forms.py
+++ src/questionario/forms.py
@@ -1,20 +1,24 @@
 from django import forms
-from questionario.models import Questionario,Pergunta
+from questionario.models import Questionario, Pergunta
 
-class QuestionarioForm(forms.ModelForm):
-    pontos = forms.IntegerField(
+respostas = [(1,1), (2,2), (3,3), (4,4), (5,5)]
+#respostas = [1, 2, 3, 4, 5]
+
+
+class QuestionarioForm(forms.Form):
+    pontos = forms.ChoiceField(
         error_messages={
             'required': 'este campo é obrigatório'},
-        widget=forms.TextInput(
+        widget=forms.Select(
             attrs={
                 'class': 'form-control',
-                'placeholder': 'Usuário',
             }
         ),
-        label=''
+        choices=respostas,
+        label='Em uma escala de 1 a 5 o quanto se identifica com esta afirmação:'
     )
 
-    class Meta():
+    '''class Meta():
         model = Pergunta
         fields = ('pontos')
 
@@ -23,4 +27,5 @@ class QuestionarioForm(forms.ModelForm):
         pontos = all_clean_data['pontos']
 
         if pontos <= 0 and pontos > 5:
-            raise forms.ValidationError("o número tem que estar no interval de 1 e 5")
+            raise forms.ValidationError(
+                "o número tem que estar no interval de 1 e 5")'''
