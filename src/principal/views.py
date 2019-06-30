740bd8c21472408870b2153f38a29c78e7457bb9
roginaldosemog <roginaldosemog@gmail.com>
2019-06-30 15:55:45 -0300

    Refatora cards index logado

--- src/principal/views.py
+++ src/principal/views.py
@@ -11,8 +11,11 @@ def index(request):
 
     if user.is_authenticated:
         usuario = User.objects.get(id=user.id)
-        artigos = Artigo.objects.order_by('-dataPublicacao')[:2]
-        context = {'usuario': usuario, 'artigos': artigos}
+
+        artigo_recomendado = Artigo.objects.order_by('-dataPublicacao').first()
+        artigos = Artigo.objects.exclude(id=artigo_recomendado.id).order_by('-dataPublicacao')[:2]
+
+        context = {'usuario': usuario, 'artigos': artigos, 'artigo_recomendado': artigo_recomendado}
         return render(request, 'index.html', context)
 
     else:
